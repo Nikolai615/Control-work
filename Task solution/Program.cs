9dfd3b1452213626ba46bbc3f5f354f287230322
Nikolai Kaboev <nik-kaboev@yandex.ru>
2022-06-21 00:09:21 +0300

    Описания решения задачи

--- Task solution/Program.cs
+++ Task solution/Program.cs
@@ -2,3 +2,30 @@
 using static System.Console;
 
 Console.Clear();
+
+string[] array1 = new string[6] {"356", "ok", "victory", "GeekBrains", "res", "45"};
+string[] array2 = new string[array1.Length];
+
+ChooseElements(array1, array2);
+PrintArray(array2);
+
+void ChooseElements(string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+    if(array1[i].Length <= 3)
+        {
+        array2[count] = array1[i];
+        count++;
+        }
+    }
+}
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
